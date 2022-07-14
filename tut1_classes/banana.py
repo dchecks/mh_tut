a3c89d7348420f69f33e737d8992f28c07dcf075
dchecks <dan.checketts@protonmail.ch>
2022-07-14 16:35:48 -0500

    Banana class example

---
+++ tut1_classes/banana.py
@@ -0,0 +1,11 @@
+
+class Banana:
+    def __init__(self, fruit_type):
+        self.fruit_type = fruit_type
+
+
+d = {"fruit_type": "YELLOw"}
+
+b = Banana()
+b.fruit_type = "Yellow"
+print(b.fruit_type)
